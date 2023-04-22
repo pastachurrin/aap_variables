b3f168afbc919f8afd21b3d7fac2c6898cb1d7da
pastachurrin <gabiel.lopez1396@gmail.com>
2023-04-22 13:48:35 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# saludo = "hola "
+# nombre = input ("ingrese su nombre: ")
+# print (saludo + nombre)
+
+"""
+Intento propio
+"""
+
+# edad = input ("ingrese su edad: ")
+
+# if(edad > 24):
+# 	print("to old for leo dicaprio")
+# else:
+# 	print("Leo dicaprio love you")
+
+"""
+trabajo 2
+"""
+
+num1 =int(input("ingrese el primer numero"))
+num2 = int(input("ingrese el segundo numero"))
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 / num2)
+print(num1 * num2)
